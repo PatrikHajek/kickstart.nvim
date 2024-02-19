dd60d42f66b395ec7faef3fc56be8d1010f1acf0
PatrikHajek <hajan21@seznam.cz>
2024-02-19 19:43:53 +0100

    vue snippets

--- init.lua
+++ init.lua
@@ -722,5 +722,30 @@ cmp.setup {
   },
 }
 
+-- snippets
+local s = luasnip.snippet
+local t = luasnip.text_node
+local i = luasnip.insert_node
+luasnip.add_snippets("vue", {
+  s("vscript", {
+    t({ "<script setup lang=\"ts\">", "" }),
+    i(1),
+    t({ "", "</script>" })
+  }),
+  s("vtemplate", {
+    t({ "<template>", "\t" }),
+    i(1),
+    t({ "", "</template>" })
+  }),
+  s("vcomponent", {
+    t({ "<script setup lang=\"ts\">", "" }),
+    i(1),
+    t({ "", "</script>", "", "<template>", "" }),
+    i(2),
+    t({ "", "</template>" }),
+  })
+})
+
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
