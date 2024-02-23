c5a14eef4481f29b0a294035b69505c558723716
PatrikHajek <hajan21@seznam.cz>
2024-02-23 19:44:39 +0100

    added Format and Write and desc

--- lua/custom/plugins/neoformat.lua
+++ lua/custom/plugins/neoformat.lua
@@ -1,6 +1,7 @@
 return {
 	'sbdchd/neoformat',
 	config = function()
-		vim.keymap.set('n', '<leader>fm', ':Neoformat<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>fm', ':Neoformat<cr>', { desc = "[F]or[m]at using Neoformat", noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>fw', ':Neoformat <BAR> :w<cr>', {desc = "[F]ormat and [W]rite", noremap = true, silent = true })
 	end
 }
