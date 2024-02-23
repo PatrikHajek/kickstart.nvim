93f331df562b64b1501968e1efed5b711141b7da
PatrikHajek <hajan21@seznam.cz>
2024-02-23 21:08:02 +0100

    conditional formattig

--- lua/custom/plugins/neoformat.lua
+++ lua/custom/plugins/neoformat.lua
@@ -1,7 +1,24 @@
+local neoformat_filetypes = {
+	'javascript', 'typescript', 'vue'
+}
+local function format()
+	if vim.tbl_contains(neoformat_filetypes, vim.bo.filetype) then
+		vim.cmd('Neoformat')
+		return
+	end
+
+	vim.lsp.buf.format()
+end
+
 return {
 	'sbdchd/neoformat',
 	config = function()
-		vim.keymap.set('n', '<leader>fm', ':Neoformat<cr>', { desc = "[F]or[m]at using Neoformat", noremap = true, silent = true })
-		vim.keymap.set('n', '<leader>fw', ':Neoformat <BAR> :w<cr>', {desc = "[F]ormat and [W]rite", noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>fm', format,
+			{ desc = "[F]or[m]at using Neoformat", noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>fw', function()
+				format()
+				vim.cmd(':w<cr>')
+			end,
+			{ desc = "[F]ormat and [W]rite", noremap = true, silent = true })
 	end
 }
