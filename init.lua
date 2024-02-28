7191eff59c0e1d6bd84843b0056f36e961df5898
PatrikHajek <hajan21@seznam.cz>
2024-02-28 20:53:53 +0100

    added lsp and treesitter servers

--- init.lua
+++ init.lua
@@ -464,7 +464,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'prisma', 'vue', 'css' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -616,6 +616,7 @@ local servers = {
   tailwindcss = {},
   -- prettierd = {},
   eslint = {},
+  prismals = {},
 
   marksman = {},
 
