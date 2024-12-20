d8bda1ba245386afadf66301f13b68f2c31a7b9c
PatrikHajek <hajan21@seznam.cz>
2024-12-20 15:02:28 +0100

    feat: keymap to replace pattern in search register

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -51,6 +51,8 @@ vim.keymap.set('n', '<CR>', function()
 end, { desc = 'Search word under the cursor' })
 vim.keymap.set('x', '<CR>', '"sy<BAR>/<C-r>s<CR>', { desc = 'Search selected text' })
 vim.keymap.set('n', '/', '/\\v', { desc = 'Enable very magic for searching', noremap = true })
+vim.keymap.set('n', '<leader>vr', ':%s//', { desc = '[V]im [R]eplace' })
+vim.keymap.set('v', '<leader>vr', ':s//', { desc = '[V]im [R]eplace in selected lines' })
 
 -- keep cursor in the middle when searching
 vim.keymap.set('n', 'n', 'nzzzv', { silent = true })
