62ab4f180eaa350e6e1e1562b0147a7a6af2e1a1
PatrikHajek <hajan21@seznam.cz>
2024-12-24 22:05:41 +0100

    feat: keymap `q` in trouble window to send items to quickfix list

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -7,6 +7,17 @@ return {
       ['<esc>'] = false,
       ['<cr>'] = 'jump_close',
       ['='] = 'fold_toggle',
+      ['q'] = {
+        action = function(view)
+          if view.opts.mode ~= 'diagnostics' then
+            print "Doesn't work for any other mode than diagnostics!"
+          end
+          local items = require('trouble').get_items()
+          vim.fn.setqflist(vim.diagnostic.toqflist(items), ' ')
+          vim.api.nvim_command ':q'
+        end,
+        desc = 'Send items to quickfix list and close the window',
+      },
     },
     -- win = {},
     -- preview = {
