c56a7c3e25179daa441f4e649ff80aef131a21ae
PatrikHajek <hajan21@seznam.cz>
2024-12-24 13:08:10 +0100

    refactor: changed `<leader>sq` to search quickfixhistory instead of quickfix

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -84,7 +84,7 @@ vim.keymap.set('n', '<leader>ld', function()
 end, { desc = 'Populate quickfix list with diagnostics and open it' })
 
 -- quickfix keymaps
-vim.keymap.set('n', '<leader>sq', ':Telescope quickfix<CR>', { desc = '[S]earch [Q]uickfix list' })
+vim.keymap.set('n', '<leader>sq', ':Telescope quickfixhistory<CR>', { desc = '[S]earch [Q]uickfix list' })
 vim.keymap.set('n', '<leader>lq', ':Trouble quickfix<CR>', { desc = 'Show quickfix list' })
 vim.keymap.set('n', '<C-n>', ':cnext<CR>', { desc = 'Go to next quickfix item' })
 vim.keymap.set('n', '<C-p>', ':cprev<CR>', { desc = 'Go to prev quickfix item' })
