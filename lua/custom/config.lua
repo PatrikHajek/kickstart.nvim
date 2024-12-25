f5121f03d8e4b8c749005c9af74bf0ae1597c0d1
PatrikHajek <hajan21@seznam.cz>
2024-12-25 22:04:29 +0100

    chore: added a note

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -58,7 +58,9 @@ vim.keymap.set('n', '<CR>', function()
   vim.api.nvim_command('/' .. word)
   vim.api.nvim_win_set_cursor(0, cursorPos)
 end, { desc = 'Search word under the cursor' })
--- NOTE: the `\z` is a lua multi-string special character - [StackOverflow](https://stackoverflow.com/a/21205005)
+-- NOTE: The `\z` is a lua multi-string special character - [StackOverflow](https://stackoverflow.com/a/21205005).
+--       This keymap can be replaced by `*` or `#` in visual mode if you don't
+--       use the multiline search.
 vim.keymap.set(
   'x',
   '<CR>',
