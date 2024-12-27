dfa11dea0defa4ae987038d114a5322d3d994850
PatrikHajek <hajan21@seznam.cz>
2024-12-27 14:42:13 +0100

    refactor: trouble lsp_references now works the same as diagnostics

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -273,15 +273,7 @@ local function goto_prev_reference()
 end
 vim.keymap.set('n', ']r', goto_next_reference, { desc = 'Go to next reference' })
 vim.keymap.set('n', '[r', goto_prev_reference, { desc = 'Go to previous reference' })
-vim.keymap.set('n', '<leader>lr', function()
-  -- NOTE: from `:help vim.lsp.listOpts`
-  vim.lsp.buf.references(nil, {
-    on_list = function(options)
-      vim.fn.setqflist({}, 'r', options)
-      vim.api.nvim_command ':Trouble quickfix'
-    end,
-  })
-end, { desc = 'Populate quickfix list with references' })
+vim.keymap.set('n', '<leader>lr', ':Trouble lsp_references<CR>', { desc = '[L]ist [R]eferences in Trouble window' })
 
 -- [[ Imported commands ]]
 require 'custom.commands.unsaved-buffers'
--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -9,12 +9,21 @@ return {
       ['='] = 'fold_toggle',
       ['q'] = {
         action = function(view)
-          if view.opts.mode ~= 'diagnostics' then
-            print "Doesn't work for any other mode than diagnostics!"
+          if view.opts.mode == 'diagnostics' then
+            local items = require('trouble').get_items()
+            vim.fn.setqflist(vim.diagnostic.toqflist(items), ' ')
+            vim.api.nvim_command ':q'
+          elseif view.opts.mode == 'lsp_references' then
+            vim.api.nvim_command ':q'
+            -- NOTE: from `:help vim.lsp.listOpts`
+            vim.lsp.buf.references(nil, {
+              on_list = function(options)
+                vim.fn.setqflist({}, 'r', options)
+              end,
+            })
+          else
+            print "Doesn't work for any other modes than diagnostics and lsp_references!"
           end
-          local items = require('trouble').get_items()
-          vim.fn.setqflist(vim.diagnostic.toqflist(items), ' ')
-          vim.api.nvim_command ':q'
         end,
         desc = 'Send items to quickfix list and close the window',
       },
