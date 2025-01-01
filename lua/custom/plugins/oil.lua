b6d9da403b2d21abc0833918378ae1feec5a81df
PatrikHajek <hajan21@seznam.cz>
2025-01-01 17:15:02 +0100

    feat: `<leader>e` in oil buffer persists directory on next open

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -14,7 +14,21 @@ return {
   config = function(_, opts)
     local oil = require 'oil'
     oil.setup(opts)
-    vim.keymap.set('n', '<leader>e', oil.toggle_float, { desc = 'Open [E]xplorer' })
+
+    local dir = nil
+    vim.keymap.set('n', '<leader>e', function()
+      if dir then
+        oil.open_float(dir)
+        dir = nil
+        return
+      end
+
+      local is_oil = vim.api.nvim_buf_get_name(0):sub(1, 6) == 'oil://'
+      if is_oil then
+        dir = oil.get_current_dir()
+      end
+      oil.toggle_float()
+    end, { desc = 'Open [E]xplorer' })
   end,
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
