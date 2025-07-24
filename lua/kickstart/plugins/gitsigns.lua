d606d5c6437f191c4b4d27510e507c04cba13463
PatrikHajek <hajan21@seznam.cz>
2025-07-24 21:54:00 +0200

    feat: enabled gitsigns numhl

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -6,6 +6,7 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+      numhl = true,
       attach_to_untracked = true,
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
