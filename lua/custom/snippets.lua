7440b1e256e2abfe31c8a4766876228b3d9aac60
PatrikHajek <hajan21@seznam.cz>
2025-09-19 01:28:17 +0200

    feat: add eslint snippets

--- lua/custom/snippets.lua
+++ lua/custom/snippets.lua
@@ -4,6 +4,15 @@ local luasnip = require 'luasnip'
 local s = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
+
+local snippets_eslint = {
+  s('esd', { t { '/* eslint-disable ' }, i(1), t { ' */' } }),
+  s('esl', { t { '// eslint-disable-next-line ' }, i(1) }),
+}
+luasnip.add_snippets('javascript', snippets_eslint)
+luasnip.add_snippets('typescript', snippets_eslint)
+luasnip.add_snippets('vue', snippets_eslint)
+
 luasnip.add_snippets('vue', {
   s('vscript', {
     t { '<script setup lang="ts">', '' },
