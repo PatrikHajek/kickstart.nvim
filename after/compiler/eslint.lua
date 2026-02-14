7c352f33c34038fa2a6dab7ae9515de2c404bc30
PatrikHajek <hajan21@seznam.cz>
2026-02-14 15:05:01 +0100

    feat: add info if the problem is an eslint warning or error

--- after/compiler/eslint.lua
+++ after/compiler/eslint.lua
@@ -2,6 +2,6 @@ vim.api.nvim_buf_set_var(0, 'current_compiler', 'eslint')
 
 -- Transform JSON into `filename:line:column: message`.
 vim.opt_local.makeprg =
-  [[pnpm eslint --max-warnings 0 --format json . | jq -r '.[] | .filePath as $f | .messages[] | "\($f):\(.line):\(.column): \(.message)"']]
+  [[pnpm eslint --max-warnings 0 --format json . | jq -r '.[] | .filePath as $f | .messages[] | "\($f):\(.line):\(.column):\(if .severity == 1 then "w" else "e" end): \(.message) [\(.ruleId)]"']]
 
-vim.opt_local.errorformat = '%f:%l:%c: %m'
+vim.opt_local.errorformat = '%f:%l:%c:%t: %m'
