ba68f3d2ab99c9146789407d166adca082c263d8
PatrikHajek <hajan21@seznam.cz>
2026-03-01 16:27:38 +0100

    fix: make `<leader>vv` work in fugitive diff

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -123,7 +123,6 @@ return {
     config = function()
       vim.keymap.set('n', '<leader>G', ':tab Git<CR> | 5G_', { desc = 'Open [G]it' })
 
-      vim.keymap.set('n', 'gfug_', '_')
       vim.api.nvim_create_autocmd({ 'BufEnter' }, {
         pattern = { 'fugitive://*' },
         callback = function(ev)
@@ -131,12 +130,12 @@ return {
             local line = vim.api.nvim_get_current_line()
             --- @type string | nil
             local line_trimmed = line:match '^[AMRD+-] *(.+)'
-            if line_trimmed then
+            if not vim.wo.diff and line_trimmed then
               --- @type string
               local char = line_trimmed:sub(1, 1)
               vim.api.nvim_command(':normal 0f' .. char)
             else
-              vim.api.nvim_command ':normal gfug_'
+              vim.api.nvim_command ':normal! _'
             end
           end, { buffer = ev.buf })
         end,
