afa495a17a44c8f9f2a84d758dff7dce2ed0f39c
PatrikHajek <hajan21@seznam.cz>
2026-03-05 00:36:38 +0100

    refactor: put keymap definitions inside init callback

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -37,24 +37,15 @@ return {
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
     branch = 'main',
+    opts = {
+      move = {
+        set_jumps = true,
+      },
+    },
     init = function()
       -- Disable entire built-in ftplugin mappings to avoid conflicts.
       -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
       vim.g.no_plugin_maps = true
-
-      vim.keymap.set({ 'n', 'x', 'o' }, '[s', function()
-        require('nvim-treesitter-textobjects.move').goto_previous_start('@local.scope', 'locals')
-      end)
-      vim.keymap.set({ 'n', 'x', 'o' }, ']s', function()
-        require('nvim-treesitter-textobjects.move').goto_next_start('@local.scope', 'locals')
-      end)
-    end,
-    config = function()
-      require('nvim-treesitter-textobjects').setup {
-        move = {
-          set_jumps = true,
-        },
-      }
 
       -- [[ Repeat ]]
       local ts_repeat_move = require 'nvim-treesitter-textobjects.repeatable_move'
@@ -69,6 +60,14 @@ return {
       vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F_expr, { expr = true })
       vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t_expr, { expr = true })
       vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T_expr, { expr = true })
+
+      -- [[ Move ]]
+      vim.keymap.set({ 'n', 'x', 'o' }, ']s', function()
+        require('nvim-treesitter-textobjects.move').goto_next_start('@local.scope', 'locals')
+      end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '[s', function()
+        require('nvim-treesitter-textobjects.move').goto_previous_start('@local.scope', 'locals')
+      end)
 
       -- [[ Select ]]
       vim.keymap.set({ 'x', 'o' }, 'as', function()
