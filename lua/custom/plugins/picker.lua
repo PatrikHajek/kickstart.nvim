d27f056308672580fed6ecbf972c3ca9cbf2b085
PatrikHajek <hajan21@seznam.cz>
2026-03-08 16:34:12 +0100

    feat: add keymaps to search commands and command history

--- lua/custom/plugins/picker.lua
+++ lua/custom/plugins/picker.lua
@@ -99,6 +99,8 @@ return {
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>s:', builtin.commands, { desc = '[S]earch [:] (commands)' })
+      vim.keymap.set('n', '<leader>sch', builtin.command_history, { desc = '[S]earch [C]ommand [H]istory' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
