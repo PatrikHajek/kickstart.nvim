5c1c1282a22b09a8311c2a59f216d438b8d29d7d
PatrikHajek <hajan21@seznam.cz>
2026-03-15 13:44:09 +0100

    refactor: add treesitter as a dependency

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -1,6 +1,9 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
     branch = 'main',
     opts = {
       move = {
